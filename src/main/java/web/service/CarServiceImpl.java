db318cc840142b3c62381d7ddc4ffab0829846fe
Daniil Loginov <daniloginov@gmail.com>
2024-12-07 22:50:14 +0500

    2-й коммит

---
+++ src/main/java/web/service/CarServiceImpl.java
@@ -0,0 +1,29 @@
+package web.service;
+
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarServiceImpl implements CarService {
+    private List<Car> cars;
+    {
+        cars = new ArrayList<>();
+
+        cars.add(new Car(1,"model1", "series1"));
+        cars.add(new Car(2,"model2", "series2"));
+        cars.add(new Car(3,"model3", "series3"));
+        cars.add(new Car(4,"model4", "series4"));
+        cars.add(new Car(5,"model5", "series5"));
+    }
+    public List<Car> getCars() {
+        return cars;
+    }
+    @Override
+    public List<Car> limitCars(List<Car> cars, int limit) {
+        if (limit > 5) {
+            return cars;
+        }
+        return cars.subList(0, limit);
+    }
+}
