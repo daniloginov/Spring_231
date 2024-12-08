6e43ea084c02cfe1ee7490d972efe7ebc1cbc457
Daniil Loginov <daniloginov@gmail.com>
2024-12-08 11:32:49 +0500

    Задача решена.

--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -13,10 +13,10 @@ public class CarServiceImpl implements CarService {
         cars = new ArrayList<>();
 
         cars.add(new Car("1", "model1", "series1"));
-        cars.add(new Car("1", "model2", "series2"));
-        cars.add(new Car("1", "model3", "series3"));
-        cars.add(new Car("1", "model4", "series4"));
-        cars.add(new Car("1", "model5", "series5"));
+        cars.add(new Car("2", "model2", "series2"));
+        cars.add(new Car("3", "model3", "series3"));
+        cars.add(new Car("4", "model4", "series4"));
+        cars.add(new Car("5", "model5", "series5"));
     }
     public List<Car> getCars() {
         return cars;
